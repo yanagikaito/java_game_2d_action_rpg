80b4dd41c8202a8f6c12ec8197837a77f91c5d4b
yanagikaito <voltgt777@gmail.com>
2025-04-29 17:19:31 +0900

    ウィンドウ表示

---
+++ src/main/java/frame/FrameFactory.java
@@ -0,0 +1,22 @@
+package frame;
+
+import window.GameWindow;
+
+import javax.swing.*;
+
+public class FrameFactory {
+
+    public static GameFrame createFrame(FrameSize size, GameWindow gameWindow) {
+        return () -> {
+            JFrame frame = new JFrame();
+            frame.setResizable(false);
+            frame.setSize(size.width(), size.height());
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setTitle("2D Action RPG");
+            frame.add(gameWindow);
+
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        };
+    }
+}
