9095653ed8ac1c8fd70c972c94962103fb638bc5
yanagikaito <voltgt777@gmail.com>
2025-05-02 20:34:05 +0900

    プレイヤーに壁や木や池に衝突用の四角形を表示させて大きさを調整して引っかかりにくくしました。

--- src/main/java/collision/CollisionChecker.java
+++ src/main/java/collision/CollisionChecker.java
@@ -2,6 +2,8 @@ package collision;
 
 import entity.Entity;
 import frame.FrameApp;
+import tile.Tile;
+import tile.TileManager;
 import window.GameWindow;
 
 public class CollisionChecker {
@@ -14,54 +16,58 @@ public class CollisionChecker {
 
     public void checkTile(Entity entity) {
 
+        int tileSize = FrameApp.getTileSize();
+
+        TileManager tileManager = gameWindow.getTileManager();
+        int[][] mapTileNum = tileManager.getMapTileNum();
+        Tile[] tiles = tileManager.getTiles();
+
         int entityLeftX = entity.getWorldX() + entity.getSolidArea().x;
         int entityRightX = entity.getWorldX() + entity.getSolidArea().x + entity.getSolidArea().width;
         int entityTopY = entity.getWorldY() + entity.getSolidArea().y;
         int entityBottomY = entity.getWorldY() + entity.getSolidArea().y + entity.getSolidArea().height;
 
-        int entityLeftCol = entityLeftX / FrameApp.createSize();
-        int entityRightCol = entityRightX / FrameApp.createSize();
-        int entityTopRow = entityTopY / FrameApp.createSize();
-        int entityBottomRow = entityBottomY / FrameApp.createSize();
+        int entityLeftCol = entityLeftX / tileSize;
+        int entityRightCol = entityRightX / tileSize;
+        int entityTopRow = entityTopY / tileSize;
+        int entityBottomRow = entityBottomY / tileSize;
 
         int tileNum1, tileNum2;
 
         switch (entity.getDirection()) {
             case "up" -> {
-                entityTopRow = (entityTopY - entity.getSpeed()) / FrameApp.createSize();
-                tileNum1 = gameWindow.getTileManager().getMapTileNum()[entityLeftCol][entityTopRow];
-                tileNum2 = gameWindow.getTileManager().getMapTileNum()[entityRightCol][entityTopRow];
-                if (gameWindow.getTileManager().getTiles()[tileNum1].collision
-                        || gameWindow.getTileManager().getTiles()[tileNum2].collision) {
+                int newTopRow = (entityTopY - entity.getSpeed()) / tileSize;
+                tileNum1 = mapTileNum[entityLeftCol][newTopRow];
+                tileNum2 = mapTileNum[entityRightCol][newTopRow];
+                if (tiles[tileNum1].collision || tiles[tileNum2].collision) {
                     entity.setCollision(true);
                 }
             }
             case "down" -> {
-                entityBottomRow = (entityBottomY + entity.getSpeed()) / FrameApp.createSize();
-                tileNum1 = gameWindow.getTileManager().getMapTileNum()[entityLeftCol][entityBottomRow];
-                tileNum2 = gameWindow.getTileManager().getMapTileNum()[entityRightCol][entityBottomRow];
-                if (gameWindow.getTileManager().getTiles()[tileNum1].collision
-                        || gameWindow.getTileManager().getTiles()[tileNum2].collision) {
+                int newBottomRow = (entityBottomY + entity.getSpeed()) / tileSize;
+                tileNum1 = mapTileNum[entityLeftCol][newBottomRow];
+                tileNum2 = mapTileNum[entityRightCol][newBottomRow];
+                if (tiles[tileNum1].collision || tiles[tileNum2].collision) {
                     entity.setCollision(true);
                 }
             }
             case "left" -> {
-                entityLeftCol = (entityLeftX - entity.getSpeed()) / FrameApp.createSize();
-                tileNum1 = gameWindow.getTileManager().getMapTileNum()[entityLeftCol][entityTopRow];
-                tileNum2 = gameWindow.getTileManager().getMapTileNum()[entityLeftCol][entityBottomRow];
-                if (gameWindow.getTileManager().getTiles()[tileNum1].collision
-                        || gameWindow.getTileManager().getTiles()[tileNum2].collision) {
+                int newLeftCol = (entityLeftX - entity.getSpeed()) / tileSize;
+                tileNum1 = mapTileNum[newLeftCol][entityTopRow];
+                tileNum2 = mapTileNum[newLeftCol][entityBottomRow];
+                if (tiles[tileNum1].collision || tiles[tileNum2].collision) {
                     entity.setCollision(true);
                 }
             }
             case "right" -> {
-                entityRightCol = (entityRightX + entity.getSpeed()) / FrameApp.createSize();
-                tileNum1 = gameWindow.getTileManager().getMapTileNum()[entityRightCol][entityTopRow];
-                tileNum2 = gameWindow.getTileManager().getMapTileNum()[entityRightCol][entityBottomRow];
-                if (gameWindow.getTileManager().getTiles()[tileNum1].collision
-                        || gameWindow.getTileManager().getTiles()[tileNum2].collision) {
+                int newRightCol = (entityRightX + entity.getSpeed()) / tileSize;
+                tileNum1 = mapTileNum[newRightCol][entityTopRow];
+                tileNum2 = mapTileNum[newRightCol][entityBottomRow];
+                if (tiles[tileNum1].collision || tiles[tileNum2].collision) {
                     entity.setCollision(true);
                 }
+            }
+            default -> {
             }
         }
     }
