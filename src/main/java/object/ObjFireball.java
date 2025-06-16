af0a7ad452686b47668f94c371bafa6e39f8f5f0
yanagikaito <voltgt777@gmail.com>
2025-06-16 20:30:47 +0900

    プレイヤーがFキー押したときにプレイヤーの方向に応じてファイアーボールを発射するようにしたいのですが,発射されずにその場で描画されたまま消えずに残ってしまいます。

---
+++ src/main/java/object/ObjFireball.java
@@ -0,0 +1,51 @@
+package object;
+
+import frame.FrameApp;
+import window.GameWindow;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import java.awt.Graphics2D;
+
+public class ObjFireball extends Projectile {
+
+    private static final String[] DIRS = {"Up", "Down", "Left", "Right"};
+    private static final int SPRITE_COUNT = 3;
+
+    public ObjFireball(GameWindow gw) {
+        super(gw, DIRS.length, SPRITE_COUNT);
+        setName("ファイアーボール");
+        setSpeed(5);
+        setMaxLife(80);
+        setAttack(2);
+        setUseCost(1);
+        setAlive(false);
+        loadSprites();    // ← 親の sprites[][] に詰め込む
+    }
+
+    @Override
+    protected void loadSprites() {
+        try {
+            int ts = FrameApp.getTileSize();
+            for (int i = 0; i < DIRS.length; i++) {
+                for (int j = 0; j < SPRITE_COUNT; j++) {
+                    String path = String.format(
+                            "projectile/image-fireball%s-%d.gif",
+                            DIRS[i], j + 1);
+                    BufferedImage ori = ImageIO.read(
+                            getClass().getClassLoader().getResourceAsStream(path)
+                    );
+                    BufferedImage buf = new BufferedImage(ts, ts, BufferedImage.TYPE_INT_ARGB);
+                    Graphics2D g = buf.createGraphics();
+                    g.drawImage(ori, 0, 0, ts, ts, null);
+                    g.dispose();
+                    sprites[i][j] = buf;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
