323a93c945e132328207e473373f5b6f5651e9b5
yanagikaito <voltgt777@gmail.com>
2026-09-05 15:15:47 +0900

    feat(world): add day-night cycle for world map

--- java_2d_game/src/main/java/environment/EnvironmentManager.java
+++ java_2d_game/src/main/java/environment/EnvironmentManager.java
@@ -8,18 +8,29 @@ public class EnvironmentManager {
 
     GameWindow gameWindow;
     Lighting lighting;
+    WorldTimeCycle timeCycle;
+    private final int lightCircleSize = 250;
 
     public EnvironmentManager(GameWindow gameWindow) {
         this.gameWindow = gameWindow;
+        this.timeCycle = new WorldTimeCycle(120.0);
     }
 
     public void setUp() {
+        lighting = new Lighting(gameWindow, lightCircleSize);
+    }
 
-        lighting = new Lighting(gameWindow, 250);
+    /**
+     * 毎フレーム呼ぶ。delta は秒（例: 1/60.0）。
+     */
+
+    public void update(double delta) {
+        timeCycle.update(delta);
+        float brightness = timeCycle.getBrightness();
+        lighting.updateAndMaybeRecreate(lightCircleSize, brightness);
     }
 
     public void draw(Graphics2D g2) {
-
         lighting.draw(g2);
     }
 }
